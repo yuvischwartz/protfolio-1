b21596b421a1c8c2309261869d198a5abe8c96b1
yuvischwartz <yuval.orderdesk@gmail.com>
2022-03-10 12:23:26 -0200

    changes

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -3,18 +3,26 @@ import '../App.css'
 import {useState, useEffect} from 'react';
 
 function Topbar() {
-  // const [click, setClick] = click = true;
+  const [click, setClick] = useState(false);
 
-  // const handleClick = () => setClick(!click);
+  const handleClick = () => {
+    setClick(!click);
+    console.log('clicked')
+  }
+
   return (
   <>
-    <div className={'hidden lg:inline-block w-screen h-9 bg-stone-400 text-sm text-center relative '}>
-      <div className={'topbar absolute'}>
+      {click && <div className={'inline-block w-screen h-9 bg-stone-400 text-sm text-center relative '}>
+        <div className={'topbar absolute'}>
           <h1 className={'text-white pt-2'}>WE SHIP PLASTIC FREE & CARBON NEUTRAL ON EVERY ORDER</h1>
-      </div>
-      <div className={'absolute right-6'}>
-        <button><i className={"fa-solid fa-xmark text-lg pt-1 text-stone-500 cursor-pointer"}></i></button></div>
-      </div>
+        </div>
+        <div className={'absolute right-6'}>
+          <i onClick={handleClick} className={"fa-solid fa-xmark text-lg pt-1 text-stone-500 cursor-pointer"}></i></div>
+      </div>}
+      {!click && <div className={'inline-block w-screen h-9 bg-stone-50 text-sm text-center relative '}>
+        <div className={'absolute right-6 text-lg pt-1 text-stone-500 cursor-pointer'}>
+          <i onClick={handleClick} className={'fa-solid fa-caret-down'}></i></div>
+      </div>}
   </>
   )
 }
